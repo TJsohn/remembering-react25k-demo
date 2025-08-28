582f84e17b2a8915206f961baee4eec6eed4006b
TJsohn <sohntaejeong@gmail.com>
2025-08-28 17:29:52 +0300

    fix some parts after 1st class

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -22,29 +22,32 @@ const Products = () => {
   return (
     <div>
         <h1>Products</h1>
-        <input type="search" placeholder="Search for a Product" value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}></input>
+        {/* <input type="search" placeholder="Search for a Product" value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}></input> */}
         <TextField id="outlined-basic" label="Outlined" variant="standard" color="secondary" value={searchTerm} 
         placeholder="Search for a Product" onChange={(e) => setSearchTerm(e.target.value)}
-        sx={{ backgroundColor: "green", borderRadius: "5px", padding: "10px" }}/>
+        sx={{ backgroundColor: "white", borderRadius: "10px", padding: "10px" }}/>
         <Grid container spacing={2} sx={{ p:2 }}>
             {filteredProducts.map((product) => (
             <Card key={product.id}
             sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
                 <div key={product.id}>
                     <h2>{product.title}</h2>
-                    <h3>Description: </h3>
+                    <h3>Description:</h3>
                     <p>{product.description}</p>
-                    <h3>Price: </h3>
-                    <p>{product.price}€</p>
-                    <h3>Category: </h3>
+                    <h3>Price:</h3>
+                    <p>{product.price} €</p>
+                    <h3>Category:</h3>
                     <p>{product.category}</p>
-                    <h3>Rating: </h3>
+                    <h3>Rating:</h3>
                     <p>{product.rating.rate}</p>
-                    <h3>Reviews: </h3>
+                    <h3>Reviews:</h3>
                     <p>{product.rating.count}</p>
-                    <h3>Image: </h3>
-                    <p><img src={product.image} alt={product.title} /></p>
+                    <h3>Image:</h3>
+                    <img 
+                    style={{objectFit: "contain", height: "300px"}}
+                    src={product.image} 
+                    alt={product.title} />
                 </div>
             </Card>
             ))}
--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
